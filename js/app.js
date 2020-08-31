e5e63e18601f8d316e6a19407b0e641c3c943606
Abid Ali <abid.saeed.ali92@gmail.com>
2020-08-31 18:02:51 +0500

    v1

--- js/app.js
+++ js/app.js
@@ -36,17 +36,42 @@ telegram.addEventListener("mouseleave", function () {
 // Modal
 
 var terms = document.querySelector("#terms");
-var policy = document.querySelector("#policy");
 var modalBox = document.querySelector("#modal-wrapper");
+var modal = document.querySelector("#modal");
 var modalClose = document.querySelector("#modal-close");
 
 terms.addEventListener("click", function () {
   modalBox.style.display = "block";
 });
-policy.addEventListener("click", function () {
-  modalBox.style.display = "block";
-});
-
 modalClose.addEventListener("click", function () {
   modalBox.style.display = "none";
+});
+modalBox.addEventListener("click", function () {
+  modalBox.style.display = "none";
+});
+modal.addEventListener("click", function () {
+  modalBox.style.display = "block";
+  console.log("hello");
+});
+
+// Modal 1
+var policy = document.querySelector("#policy");
+var modalBox1 = document.querySelector("#modal-wrapper-1");
+var modalClose1 = document.querySelector("#modal-close-1");
+
+policy.addEventListener("click", function () {
+  modalBox1.style.display = "block";
+});
+modalClose1.addEventListener("click", function () {
+  modalBox1.style.display = "none";
+});
+modalBox1.addEventListener("click", function () {
+  modalBox1.style.display = "none";
+});
+
+// Form
+
+var form = document.querySelector("#form");
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
 });
